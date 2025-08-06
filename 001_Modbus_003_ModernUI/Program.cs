ed1c05234eff10aaa39a38f6d152bbd0ac1c04c5
Tran Quang Duc Hoa <duchoadcgl@gmail.com>
2025-08-06 09:46:07 +0700

    test commit

--- 001_Modbus_003_ModernUI/Program.cs
+++ 001_Modbus_003_ModernUI/Program.cs
@@ -17,6 +17,7 @@ namespace _001_Modbus_003_ModernUI
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+            Application.Run(new Form1());
         }
     }
 }
